024944d4475e67b52a079791e5c4b822df3a6e64
Tonmoy Talukder <tonmoytalukder2000@gmail.com>
2024-11-15 04:23:30 +0600

    Removed next.config.ts

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
     reactStrictMode: true,
     // Other configurations...
+    env: {
+        NEXT_PUBLIC_GEMINI_API_KEY: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
+        NEXT_PUBLIC_HUGGINGFACE_TOKEN: process.env.NEXT_PUBLIC_HUGGINGFACE_TOKEN,
+      },
   };
   
   export default nextConfig;
