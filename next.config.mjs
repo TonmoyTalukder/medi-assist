c2370787718aacc982822ba17a347da2324a21c8
Tonmoy Talukder <tonmoytalukder2000@gmail.com>
2024-11-15 03:19:09 +0600

    Removed next.config.ts

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  // Other configurations...
-};
-
-export default nextConfig;
+    reactStrictMode: true,
+    // Other configurations...
+  };
+  
+  export default nextConfig;
+  
